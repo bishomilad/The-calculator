919fcb71f91e944177fc62b31d0c291dbb0236e2
Bishoy Milad <123456789+bishomilad72@yahoo.com>
2026-03-14 04:16:20 +0200

    Fixing the whole calculator logic, adding history

--- calculator.js
+++ calculator.js
@@ -1,18 +1,103 @@
-
-//user clicks numbers -> until operator pressed -> save num and operator, wait for new num ->
+"use strict";
 
 function Calculator(){
     const calc = {};
     calc.queue =[];
-    calc.history=[];
-    calc.currentNum=0;
+    calc.hist = new Array();
+    calc.lastNum=null;
+    calc.lastOp=null;
     calc.numBtnsContainer = document.querySelector(".numBtnsContainer");
     calc.opBtnsContainer = document.querySelector(".operationBtnsContainer");
-    calc.paper = document.querySelector(".paper");
-    calc.calculate = function (operator){
+    calc.paper = document.querySelector(".paperContents");
+    calc.lastNumEl = document.querySelector("#lastNum");
+    calc.resultNumEl = document.querySelector("#resultNum");
+
+    calc.calculate = function (clicked){
+         //preventing calculating without parameters
         let num = +this.queue.join("");
+        console.log(this.hist);
+        //resets the queue to enable the user from doing operations on two numbers
+        this.queue.splice(0, this.queue.length);
+        this.lastNum= this.lastNum===null? num: this.lastNum;
+        if(this.lastOp ===null){
+            this.lastOp= clicked;
+            this.resultNumEl.textContent = num ;
+            return;
+        }
+        let result=0;
+        switch (this.lastOp){
+            case '+':
+                result = this.lastNum + num;
+                this.addToPaper(this.lastNum, num, this.lastOp, result);
+                break;
+            case '-':
+                result = this.lastNum - num;
+                this.addToPaper(this.lastNum, num, this.lastOp, result);
+                break;
+            case '*':
+                result = this.lastNum * num;
+                this.addToPaper(this.lastNum, num, this.lastOp, result);
+                break;
+            case '/':
+                result = this.lastNum / num;
+                this.addToPaper(this.lastNum, num, this.lastOp, result);
+                break;
+            case '=':
+                result = num!==this.lastNum? num: this.lastNum;
+        }
 
+        switch(clicked){
+            case '=':
+                this.displayNums(result?? this.lastNum);
+                this.lastNum= result?? this.lastNum;
+                this.lastOp = null;
+                this.queue = String(this.lastNum).split();
+                break;
+                return;
+            case 'AC':
+                this.queue.splice(0,this.queue.length);
+                this.lastNum=null;
+                this.lastOp=null;
+                this.displayNums(0);
+                return;
+                break;
+            case "C":
+                this.queue.pop();
+                this.displayNums();
+                return;
+                break;
+            default:
+                this.lastOp = clicked;
+        }
+        
+        this.lastNum = result;
+        this.displayNums(result);
+        this.lastOp = clicked;
+    };
 
+    calc.pressNum = function(num){
+        switch (num){
+            case ".":
+                if(this.queue.includes(".")){
+                    return;
+                }
+                break;
+            case "+-":
+                if(this.queue[0]=== "-"){
+                    this.queue.shift()
+                }else this.queue.unshift("-");
+                this.displayNums();
+                return;
+                break;
+        }
+        this.queue.push(num);
+        this.displayNums(this.queue.join(""));
+    }
+
+    calc.displayNums = function(result= this.queue.join("")){
+        if(result === "") result=0;
+        this.lastNumEl.textContent = this.lastNum?? 0;
+        this.resultNumEl.textContent = result;
     };
 
     calc.displayNumBtns = function (){
@@ -26,13 +111,9 @@ function Calculator(){
         let numBtnsRow = document.createElement("div");
         calc.createBtn(0,0,numBtnsRow,0);
         calc.createBtn(0,0,numBtnsRow,".");
+        calc.createBtn(0,0,numBtnsRow,"+-");
         numBtnsRow.classList.add("lastNumRow");
         this.numBtnsContainer.insertAdjacentElement("beforeend",numBtnsRow);
-
-
-
-
-    
     };
 
     calc.createBtn = function(i, j, numBtnsRow, num=null){
@@ -65,31 +146,46 @@ function Calculator(){
     };
     calc.addToPaper = function(num1, num2, op, result){
         let expression = `${num1} ${op} ${num2} = ${result}`;
-        //add string to history array
-        calc.history.push(expression);
-        //reset the history paper
-        calc.UpdatePaper();
+        //add string to hist array
+        this.hist.push(expression);
+        //reset the hist paper
+        this.updatePaper(expression, this.hist.length-1);
         //add string to localstrorage
-        
+        localStorage.setItem("hist",JSON.stringify(this.hist));
 
     };
-    calc.UpdatePaper = function (){
 
-        calc.history?.forEach((exp, i)=>{
+    calc.updatePaper = function(expression,i){
             const element = document.createElement("p");
             element.setAttribute("data-id",i);
+            element.textContent = expression
             element.classList.add("expression");
-            calc.paper.firstChild.insertAdjacentElement("afterend");
+            this.paper.insertAdjacentElement("afterbegin",element);
+
+    }
+
+    calc.displaySavedPaper = function (){
+        if(localStorage.getItem("hist")===null) return;
+        this.hist = new Array(JSON.parse(localStorage.getItem("hist")));
+        this.hist?.forEach((exp, i)=>{
+            this.updatePaper(exp,i)
         })
         
     }
-
     return calc;
 };
 
 const calculator = new Calculator();
-
+calculator.displaySavedPaper();
 calculator.displayBtns();
+calculator.opBtnsContainer.addEventListener("click",e =>{
+    if(e.target.tagName === "BUTTON") calculator.calculate(e.target.dataset.op);
+});
+    
+calculator.numBtnsContainer.addEventListener("click", e =>{
+    if(e.target.tagName === "BUTTON") calculator.pressNum(e.target.dataset.num);
+});
+
 
 
 
